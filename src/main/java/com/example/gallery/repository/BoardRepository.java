b7923057aa66136f459563cf5a0a882a7da3a32f
youdam <sone5352@gmail.com>
2022-12-30 00:26:52 +0900

    test

--- src/main/java/com/example/gallery/repository/BoardRepository.java
+++ src/main/java/com/example/gallery/repository/BoardRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /* JpaRepository옆에 보면
     JpaRepository<T, ID> 이렇게 되어있음. Type 은 board(게시글이니까)
     id  는 뭐였노 long이였제?
+
+    git !!
 * */
 
 
